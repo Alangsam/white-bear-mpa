861de5e1c7e37634971c0af9950906a33ac64549
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-06-04 12:23:21 -0700

    added pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
+import "./style/master.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AllCards from "./components/pages/AllCards";
+import CreateAnswer from "./components/pages/CreateAnswer";
+import CreateImagery from "./components/pages/CreateImagery";
+import Edit from "./components/pages/Edit";
+import Landing from "./components/pages/Landing";
+import ReviewAnswer from "./components/pages/ReviewAnswer";
+import ReviewEmpty from "./components/pages/ReviewEmpty";
+import ReviewImagery from "./components/pages/ReviewImagery";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
    return (
-      <div className="App">
-         <h1>hello World</h1>
-      </div>
+      <Router>
+         <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/create-answer" component={CreateAnswer} />
+            <Route exact path="/create-imagery" component={CreateImagery} />
+            <Route exact path="/review-imagery" component={ReviewImagery} />
+            <Route exact path="/review-answer" component={ReviewAnswer} />
+            <Route exact path="/review-empty" component={ReviewEmpty} />
+            <Route exact path="/all-cards" component={AllCards} />
+            <Route exact path="/edit" component={Edit} />
+            <Route component={NotFound} />
+         </Switch>
+      </Router>
    );
 }
 
