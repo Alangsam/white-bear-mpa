bcf8584baaf9114f831e6c0354e67bf32ecf9b47
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-06-10 11:20:53 -0700

    added data schemas

---
+++ src/mock-data.js/memory-cards.js
@@ -0,0 +1,51 @@
+const memoryCards = [
+   {
+      id: "8e9395c5-e115-41e4-a5b4-76ff2d766ac5",
+      imagery:
+         "Dashwood contempt on mr unlocked resolved provided of of. Stanhill wondered it it welcomed oh. Hundred no prudent he however smiling at an offence.",
+      answer:
+         "If earnestly extremity he he propriety something admitting convinced ye.",
+      userId: "42160c3e-7a5b-4fb9-b361-ac9598aca4e2",
+      createdAt: 1591811787,
+      nextAttemptAt: 1591812427,
+      lastAttemptAt: 1591811787,
+      totalSuccessfulAttempts: 0,
+      level: 1,
+   },
+   {
+      id: "b195e340-8631-4cdd-a81c-64207653edd2",
+      imagery:
+         "At ourselves direction believing do he departure. Celebrated her had sentiments understood are projection set. Possession ye no mr unaffected remarkably at.",
+      answer: "Wrote house in never fruit up.",
+      userId: "42160c3e-7a5b-4fb9-b361-ac9598aca4e2",
+      createdAt: 1591812235,
+      nextAttemptAt: 1591812930,
+      lastAttemptAt: 1591812342,
+      totalSuccessfulAttempts: 0,
+      level: 1,
+   },
+   {
+      id: "d5d05674-34c4-4ac8-b023-10ee7f5567a2",
+      imagery:
+         "Who connection imprudence middletons too but increasing celebrated principles joy. Herself too improve gay winding ask expense are compact. New all paid few hard pure she.",
+      answer: "Able an hope of body.",
+      userId: "42160c3e-7a5b-4fb9-b361-ac9598aca4e2",
+      createdAt: 1591813787,
+      nextAttemptAt: 1591814834,
+      lastAttemptAt: 1591813987,
+      totalSuccessfulAttempts: 0,
+      level: 1,
+   },
+   {
+      id: "db8150ec-f314-4fea-8b5b-6770dcb7b68a",
+      imagery:
+         "On lasted uneasy exeter my itself effect spirit. At design he vanity at cousin longer looked ye. Design praise me father an favour. As greatly replied it windows of an minuter behaved passage.",
+      answer: "Open know age use whom him than lady was.",
+      userId: "42160c3e-7a5b-4fb9-b361-ac9598aca4e2",
+      createdAt: 1591814787,
+      nextAttemptAt: 1591815737,
+      lastAttemptAt: 1591814987,
+      totalSuccessfulAttempts: 0,
+      level: 1,
+   },
+];
---
+++ src/mock-data.js/users.js
@@ -0,0 +1,14 @@
+const users = [
+   {
+      id: "42160c3e-7a5b-4fb9-b361-ac9598aca4e2",
+      email: "blah@email.com",
+      password: "0f317e5c7cbbbd8c834a68228ba597f8",
+      createdAt: 1591811684,
+   },
+   {
+      id: "69b0a584-21f9-4fa3-a68d-0b5af2d29872",
+      email: "ohBoy@aol.com",
+      password: "491c74c905a1f904c0a696391d47c139",
+      createdAt: 1591841685,
+   },
+];
