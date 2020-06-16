dfbb6aa090d85c37ddacd4ed9f096b51efdad4fc
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-06-15 18:58:51 -0700

    added search and sort to all cards

--- src/components/pages/AllCards.js
+++ src/components/pages/AllCards.js
@@ -2,49 +2,101 @@ import React from "react";
 import AppTemplate from "../ui/AppTemplate";
 import { memoryCards } from "../../mock-data/memory-cards";
 import MemoryCard from "../ui/MemoryCard";
+import { orderBy } from "lodash";
 
-export default function AllCards() {
-   return (
-      <AppTemplate>
-         <div className="row my-4">
-            <div className="col-8">
-               <input
-                  id="search-this-box"
-                  className="form-control form-control-sm border-primary "
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-               />
+export default class AllCards extends React.Component {
+   constructor() {
+      super();
+      this.state = {
+         whatSearched: "",
+         cardsRendered: orderBy([...memoryCards], "lastAttemptAt", "desc"),
+      };
+   }
+
+   filterCardsbySearch() {
+      const arrOfMatches = memoryCards.filter((card) => {
+         if (
+            card.answer
+               .toLowerCase()
+               .indexOf(this.state.whatSearched.toLowerCase()) > -1 ||
+            card.imagery
+               .toLowerCase()
+               .indexOf(this.state.whatSearched.toLowerCase()) > -1
+         ) {
+            return card;
+         }
+      });
+      this.setState({
+         cardsRendered: orderBy([...arrOfMatches], "lastAttemptAt", "desc"),
+      });
+   }
+
+   filterCardsBySelect(e) {
+      const arrOfSelected = [...this.state.cardsRendered];
+      const orderedByRecent = orderBy(arrOfSelected, "lastAttemptAt", "desc");
+      const orderedByOldest = orderBy(arrOfSelected, "lastAttemptAt", "asc");
+      if (e.target.value === "Most recent") {
+         this.setState({ cardsRendered: orderedByRecent });
+      } else if (e.target.value === "Oldest") {
+         this.setState({ cardsRendered: orderedByOldest });
+      }
+   }
+
+   render() {
+      return (
+         <AppTemplate>
+            <div className="row my-4">
+               <div className="col-8">
+                  <input
+                     id="search-this-box"
+                     className="form-control form-control-sm border-primary "
+                     type="search"
+                     placeholder="Search"
+                     aria-label="Search"
+                     onChange={(e) => {
+                        this.setState({ whatSearched: e.target.value });
+                        console.log(this.state.whatSearched);
+                     }}
+                  />
+               </div>
+               <div className="col-4">
+                  <button
+                     id="use-this-btn-to-search"
+                     className="btn btn-sm btn-primary text-white btn-block"
+                     onClick={() => {
+                        this.filterCardsbySearch();
+                     }}
+                  >
+                     Search
+                  </button>
+               </div>
             </div>
-            <div className="col-4">
-               <button
-                  id="use-this-btn-to-search"
-                  className="btn btn-sm btn-primary text-white btn-block"
+            <div className="row pt-4 no-gutters">
+               <p className="col-4">search cards by</p>
+               <select
+                  id="sort"
+                  className="form-control border-primary col-8 float-right w-50 pt-0 "
+                  onChange={(e) => {
+                     this.filterCardsBySelect(e);
+                     console.log(e.target.value);
+                  }}
                >
-                  Search
-               </button>
+                  <option className="" defaultValue>
+                     Most recent
+                  </option>
+                  <option>Oldest</option>
+               </select>
             </div>
-         </div>
-         <div className="row pt-4 no-gutters">
-            <p className="col-4">search cards by</p>
-            <select
-               id="sort"
-               className="form-control border-primary col-8 float-right w-50 pt-0 "
-            >
-               <option className="" selected>
-                  Most recent
-               </option>
-            </select>
-         </div>
-         {memoryCards.map((memoryCard) => {
-            return (
-               <MemoryCard
-                  imagery={memoryCard.imagery}
-                  answer={memoryCard.answer}
-                  key={memoryCard.id}
-               />
-            );
-         })}
-      </AppTemplate>
-   );
+            {this.state.cardsRendered.map((memoryCard) => {
+               return (
+                  <MemoryCard
+                     imagery={memoryCard.imagery}
+                     answer={memoryCard.answer}
+                     key={memoryCard.id}
+                  />
+               );
+            })}
+         </AppTemplate>
+      );
+   }
 }
